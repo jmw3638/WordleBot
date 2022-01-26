08d65e45ff37c9281983fe0c7b9f525e75df2deb
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2022-01-26 00:44:28 -0500

    Added argument to build letter coords dict from enter button coords

--- wordle.py
+++ wordle.py
@@ -1,37 +1,6 @@
 import argparse
-from tracemalloc import start
 import pyautogui
 import time
-
-close_init_prompt = (-740, 328)
-letter_dict = {
-    'a': (-1160, 926),
-    'b': (-912, 990),
-    'c': (-1010, 990),
-    'd': (-1060, 926),
-    'e': (-1083, 862),
-    'f': (-1010, 926),
-    'g': (-960, 926),
-    'h': (-912, 926),
-    'i': (-838, 862),
-    'j': (-860, 926),
-    'k': (-813, 926),
-    'l': (-762, 926),
-    'm': (-813, 990),
-    'n': (-860, 990),
-    'o': (-789, 862),
-    'p': (-740, 862),
-    'q': (-1182, 862),
-    'r': (-1034, 862),
-    's': (-1109, 926),
-    't': (-985, 862),
-    'u': (-887, 862),
-    'v': (-960, 990),
-    'w': (-1132, 862),
-    'x': (-1060, 990),
-    'y': (-937, 862),
-    'z': (-1109, 990)
-}
 
 def parse_args(parser):
     parser.add_argument('-v', '--verbose',
@@ -42,17 +11,46 @@ def parse_args(parser):
     parser.add_argument('-s', '--start',
             help='choose a word to start the puzzle with')
 
+    parser.add_argument('-c', '--coords',
+            help='screen coords of center of \'enter\' button, format as \'-cx,y\' \'--coords=x,y\'',
+            required=True)
+
     return parser.parse_args()
 
 def vlog(message, level=1):
     if log_level >= level:
         print(message)
 
+def build_letter_dict(coords):
+    vlog('Enter button coords: {}'.format(coords))
+    letter_dict = {}
+
+    rel_coords = (coords[0] + 60, coords[1])
+    for l in 'zxcvbnm':
+        letter_dict[l] = rel_coords
+        vlog('Added \'{}\' to dict with coords {}'.format(l, rel_coords), 2)
+        rel_coords = (rel_coords[0] + 50, rel_coords[1])
+
+    rel_coords = (rel_coords[0], rel_coords[1] - 65)
+    for l in 'lkjhgfdsa':
+        letter_dict[l] = rel_coords
+        vlog('Added \'{}\' to dict with coords {}'.format(l, rel_coords), 2)
+        rel_coords = (rel_coords[0] - 50, rel_coords[1])
+
+    rel_coords = (rel_coords[0] + 30, rel_coords[1] - 65)
+    for l in 'qwertyuiop':
+        letter_dict[l] = rel_coords
+        vlog('Added \'{}\' to dict with coords {}'.format(l, rel_coords), 2)
+        rel_coords = (rel_coords[0] + 50, rel_coords[1])
+
+    vlog(letter_dict, 3)
+    return letter_dict
+
 def click(pos):
     pyautogui.click(pos)
     vlog('Left mouse button pressed at {}'.format(pos))
 
-def enter_word(word):
+def enter_word(word, letter_dict):
     if not len(word) == 5:
         return False
 
@@ -62,7 +60,7 @@ def enter_word(word):
             return False
 
     for letter in word:
-        vlog('Submitting letter: {}'.format(letter_dict[letter]))
+        vlog('Submitting letter: {}'.format(letter))
         click(letter_dict[letter])
     return True
 
@@ -72,21 +70,26 @@ def main():
     global log_level
     log_level = args.verbose
 
-    vlog(args)
-    
+    vlog(args, 2)
+
+    str_coords = args.coords
+    split_coords = str_coords.split(',')
+    coords = (int(split_coords[0]), int(split_coords[1]))
+    letter_dict = build_letter_dict(coords)
+
     # Sleep to allow user to open Worldle game
     # https://www.powerlanguage.co.uk/wordle/
     time.sleep(3)
     vlog('Sleep complete, continuing program')
 
-    click(close_init_prompt)
+    click(coords)
     time.sleep(0.5)
     
     starting_word = 'hello'
     if args.start:
         starting_word = args.start
         
-    if not enter_word(starting_word):
+    if not enter_word(starting_word, letter_dict):
         print('Error: failed to enter word \'{}\''.format(starting_word))
 
 if __name__ == "__main__":
